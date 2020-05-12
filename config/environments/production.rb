a57cbb848aad9d471660d0c09cc91d26afa7a5e6
MpaK <mrak69@gmail.com>
2020-05-12 12:35:14 +0500

    Host for Production;

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,4 +105,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  # config.action_controller.default_url_options = { host: 'localhost', port: 5000 }
+  Rails.application.routes.default_url_options[:host] = 'instaclone6.herokuapp.com'
 end
